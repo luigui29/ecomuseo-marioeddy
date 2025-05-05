cad9b45cd6a248aa3950d60f255c4653231d73e8
luigui29 <luiguigarrido29@gmail.com>
2025-05-05 09:56:28 -0400

    chore: cambios para que funcione el router con vercel, prueba 1

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import Card from '../Components/Card.jsx'
-import img from '../assets/ecomuseo-carousel-2.jpg'
+
+//Titeres
+
+import dragonflojo_img from '../assets/dragondienteflojo-img1.jpg'
+import avolarpapagayo_img from '../assets/avolarpapagayo-img1.jpg'
+import meloencontreprim_img from '../assets/meloencontreprimero-img1.jpg'
 
 const Collection = () => {
   return (
-    <div className='container mt-5 mb-5 mx-5 me-5'>
-      <h2 className='text-center mb-5'>Colección</h2>
-       <Card title="Card title " description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-       <img src={img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
-       <Card title="Card title" description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-       <img src={img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
-       <Card title="Card title" description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-       <img src={img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
-       <Card title="Card title" description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-       <img src={img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
-       <Card title="Card title" description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
-       <img src={img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
-    </div>
+    <main className='museum-background'>
+      <div className='container mx-5 me-5'>
+        <h2 className='text-center mb-5'>Colección</h2>
+        <Card title="Card title " description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+        <img src={dragonflojo_img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
+        <Card title="Card title" description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+        <img src={avolarpapagayo_img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
+        <Card title="Card title" description="Some quick example text to build on the card title and make up the bulk of the card's content."/>
+        <img src={meloencontreprim_img} alt="" className='img-fluid mb-5 w-50 mx-auto'/>
+      </div>
+    </main>
   )
 }
 
